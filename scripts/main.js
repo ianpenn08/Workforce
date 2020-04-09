8213de4725d3c5e61a1ff9d7ef17b9fd8f956237
ianpenn08 <inkmpenn@gmail.com>
2020-04-09 10:24:11 -0500

    added departments

--- scripts/main.js
+++ scripts/main.js
@@ -1,10 +1,12 @@
 import { getEmployees } from "./Employees/employeeDataProvider.js";
 import { getComputers } from "./computerDataProvider.js";
 import {employeeList} from "./Employees/employeeList.js";
+import { getDepartments } from "../Department/departmentDataProvider.js";
 
 
 getEmployees()
     .then(getComputers)
+    .then(getDepartments)
     .then(employeeList)
 
 
